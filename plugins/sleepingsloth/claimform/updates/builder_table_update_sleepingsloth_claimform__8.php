259a445145a4734278157396b7dbf00f8696688a
Zenon Chrysostomou <zenon1020@me.com>
2019-01-20 05:45:51 -0700

    Fixed flow, removed step. Final

---
+++ plugins/sleepingsloth/claimform/updates/builder_table_update_sleepingsloth_claimform__8.php
@@ -0,0 +1,23 @@
+<?php namespace Sleepingsloth\Claimform\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateSleepingslothClaimform8 extends Migration
+{
+    public function up()
+    {
+        Schema::table('sleepingsloth_claimform_', function($table)
+        {
+            $table->smallInteger('incident_conclusion');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('sleepingsloth_claimform_', function($table)
+        {
+            $table->dropColumn('incident_conclusion');
+        });
+    }
+}
---
+++ plugins/sleepingsloth/claimform/updates/builder_table_update_sleepingsloth_claimform__9.php
@@ -0,0 +1,23 @@
+<?php namespace Sleepingsloth\Claimform\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateSleepingslothClaimform9 extends Migration
+{
+    public function up()
+    {
+        Schema::table('sleepingsloth_claimform_', function($table)
+        {
+            $table->text('incident_conclusion')->nullable(false)->unsigned(false)->default(null)->change();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('sleepingsloth_claimform_', function($table)
+        {
+            $table->smallInteger('incident_conclusion')->nullable(false)->unsigned(false)->default(null)->change();
+        });
+    }
+}
