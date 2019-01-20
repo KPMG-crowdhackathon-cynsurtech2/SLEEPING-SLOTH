6ff8aebf90ba920b23c5a5124965eb5139c649ff
Zenon Chrysostomou <zenon1020@me.com>
2019-01-19 18:22:37 -0700

    modified DB of claims to have a unique id

---
+++ plugins/sleepingsloth/claimform/updates/builder_table_update_sleepingsloth_claimform__4.php
@@ -0,0 +1,23 @@
+<?php namespace Sleepingsloth\Claimform\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateSleepingslothClaimform4 extends Migration
+{
+    public function up()
+    {
+        Schema::table('sleepingsloth_claimform_', function($table)
+        {
+            $table->text('unique_id');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('sleepingsloth_claimform_', function($table)
+        {
+            $table->dropColumn('unique_id');
+        });
+    }
+}
